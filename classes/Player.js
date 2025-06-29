4f90047e429d0fb59f3b5cef9f0c733d7fd61508
Joel Kram <joelnskram@gmail.com>
2025-06-29 12:48:01 +0100

    init

---
+++ classes/Player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(currentHp, maxHp, gold, currentXp, currentLevel) {
+    this.currentHp = currentHp;
+    this.maxHp = maxHp;
+    this.gold = gold;
+    this.currentXp = currentXp;
+    this.currentLevel = currentLevel;
+  }
+}
